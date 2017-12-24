17a7a164e3d1008fc3e1894c895b9c0fcdfed6cb
Joshua Harrowell <iam@joshharrowell.co.uk>
2017-12-24 19:36:18 +0000

    Added basic comparer methods, installed scrapy and renamed project to specsfyi

--- comparer.py
+++ comparer.py
@@ -1,5 +1,19 @@
 from flask import jsonify
 
 def comparer(data):
-    monthly_total = data['phone_contract']
-    return monthly_total
+    contract_monthly_total = data['phone_contract']['monthly'] * 24
+    contract_total = contract_monthly_total + data['phone_contract']['upfront']
+
+    sim_monthly_total = data['sim_contract']['monthly'] * 24
+    sim_total = sim_monthly_total + data['sim_contract']['handset_cost']
+
+    return jsonify({
+        'network': data['network'],
+        'post_code': data['post_code'],
+        'phone_contract': {
+            'contract_total': contract_total
+        },
+        'sim_contract': {
+            'sim_total': sim_total
+        },
+    })
